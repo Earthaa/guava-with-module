53eb69ca27857c9afd2690bb001fc5199b96d4cf
Earthaa <2892212@qq.com>
2018-11-19 17:42:46 -0800

    Initial commit

---
+++ guava-gwt/src/module-info.java
@@ -0,0 +1,8 @@
+module guava.gwt {
+    requires checker.qual;
+    requires error.prone.annotations;
+   //requires gwt.dev;
+    requires gwt.user;
+    requires  guava;
+
+}
---
+++ guava-testlib/src/module-info.java
@@ -0,0 +1,9 @@
+module guava.testlib {
+    requires java.compiler;
+    requires java.logging;
+    requires checker.qual;
+    requires error.prone.annotations;
+    requires junit;
+    requires guava;
+//    requires guava.gwt;
+}
